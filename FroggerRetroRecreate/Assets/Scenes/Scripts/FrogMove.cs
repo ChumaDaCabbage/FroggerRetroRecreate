5084433de0de43f6dc3c9c9243eaf0bf8fce5243
MUKILTEO\1707224 <1707224@Mukilteo.wednet.edu>
2021-06-03 13:17:35 -0700

    Frog basic movement

--- FroggerRetroRecreate/Assets/Scenes/Scripts/FrogMove.cs
+++ FroggerRetroRecreate/Assets/Scenes/Scripts/FrogMove.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class FrogMove : MonoBehaviour
 {
+
+    public Sprite turnedFrog;
+    Sprite defualtFrog;
+
+    SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = gameObject.GetComponent<SpriteRenderer>();
+
+        defualtFrog = sr.sprite;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            sr.sprite = defualtFrog;
+            sr.flipY = false;
+            transform.position += new Vector3(0, 0.6625512f, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            sr.sprite = defualtFrog;
+            sr.flipY = true;
+            transform.position += new Vector3(0, -0.6625512f, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            sr.sprite = turnedFrog;
+            sr.flipX = true;
+            transform.position += new Vector3(-0.88888888888f, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            sr.sprite = turnedFrog;
+            sr.flipX = false;
+            transform.position += new Vector3(0.88888888888f, 0, 0);
+        }
     }
 }
