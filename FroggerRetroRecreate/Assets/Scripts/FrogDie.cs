6f73441606a469dd7eedb9a6d4b51911556cffc2
ChumaDaCabbage <1707224@Mukilteo.wednet.edu>
2021-06-07 10:45:49 -0700

    Added Lives system and death

---
+++ FroggerRetroRecreate/Assets/Scripts/FrogDie.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogDie : MonoBehaviour
+{
+
+    public Sprite frogDeath;
+    public Sprite defualtSprite;
+
+    public GameObject[] livesBar = new GameObject[4];
+
+    int lives = 5;
+
+    Vector3 startPos;
+
+    SpriteRenderer sr;
+    FrogMove fm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        fm = gameObject.GetComponent<FrogMove>();
+
+        startPos = transform.position;
+    }
+
+    public void startDeath()
+    {
+        lives--;
+        if (lives != 0)
+        {
+            livesBar[lives - 1].SetActive(false);
+        }
+
+        fm.enabled = false;
+        sr.flipY = false;
+        sr.flipX = false;
+        sr.sprite = frogDeath;
+        StartCoroutine(deathPause());
+    }
+
+    public IEnumerator deathPause()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        if (lives > 0)
+        {
+            transform.position = startPos;
+            sr.sprite = defualtSprite;
+            fm.enabled = true;
+        }
+        else
+        {
+            sr.enabled = false;
+        }
+    }
+}
---
+++ FroggerRetroRecreate/Assets/Scripts/FrogMove.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogMove : MonoBehaviour
+{
+    public Sprite turnedFrog;
+    Sprite defualtFrog;
+
+    SpriteRenderer sr;
+    FrogDie fd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        fd = gameObject.GetComponent<FrogDie>();
+
+        defualtFrog = sr.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, 0.6625512f);
+            Debug.DrawRay(transform.position, transform.up, Color.white, 10);
+            if (hit == false)
+            {
+                transform.position += new Vector3(0, 0.6625512f, 0);
+            }
+
+            if (hit != false && hit.transform.tag == "Death")
+            {
+                fd.startDeath();
+            }
+            else
+            {
+                sr.sprite = defualtFrog;
+                sr.flipY = false;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, 0.6625512f);
+            Debug.DrawRay(transform.position, -transform.up, Color.white, 10);
+            if (hit == false)
+            {
+                transform.position += new Vector3(0, -0.6625512f, 0);
+            }
+
+            if (hit != false && hit.transform.tag == "Death")
+            {
+                fd.startDeath();
+            }
+            else
+            {
+                sr.sprite = defualtFrog;
+                sr.flipY = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.right, 0.88888888888f);
+            Debug.DrawRay(transform.position, -transform.right, Color.white, 10);
+            if (hit == false)
+            {
+                transform.position += new Vector3(-0.88888888888f, 0, 0);
+            }
+
+            if (hit != false && hit.transform.tag == "Death")
+            {
+                fd.startDeath();
+            }
+            else
+            {
+                sr.sprite = turnedFrog;
+                sr.flipX = true;
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 0.88888888888f);
+            Debug.DrawRay(transform.position, transform.right, Color.white, 10);
+            if (hit == false)
+            {
+                transform.position += new Vector3(0.88888888888f, 0, 0);
+            }
+
+            if (hit != false && hit.transform.tag == "Death")
+            {
+                fd.startDeath();
+            }
+            else
+            {
+                sr.sprite = turnedFrog;
+                sr.flipX = false;
+            }
+        }
+    }
+}
